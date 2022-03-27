eb2a3988945c8e4d37ec9cc1843118490c541f4e
p0js <ztraktor2@gmail.com>
2022-03-27 14:50:00 +0200

    Dodanie platformy gracza

--- main.c
+++ main.c
@@ -3,16 +3,23 @@
 #include <ctype.h>
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+/*
+    Dokumentacja Allegro5
+    https://github.com/liballeg/allegro_wiki/wiki/Allegro-Vivace%3A-Basic-game-structure
+*/
+
 
 int main()
 {
     al_init();
     al_install_keyboard();
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
     ALLEGRO_DISPLAY* disp = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
     ALLEGRO_FONT* font = al_create_builtin_font();
@@ -24,20 +31,35 @@ int main()
     bool redraw = true;
     ALLEGRO_EVENT event;
 
+    if(!al_init_image_addon()) {
+        printf("Inicjalizaja obrazow nie powiodla sie.\n");
+        return 1;
+    }
+
+    //Obraz platformy gracza
+    ALLEGRO_BITMAP* gracz = al_load_bitmap("platforma_gracza.png");
+
+
     al_start_timer(timer);
     while(1)
     {
         al_wait_for_event(queue, &event);
 
-        if(event.type == ALLEGRO_EVENT_TIMER)
+        if (event.type == ALLEGRO_EVENT_TIMER) {
             redraw = true;
-        else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+        }
+        else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             break;
+        }
 
-        if(redraw && al_is_event_queue_empty(queue))
+        if (redraw && al_is_event_queue_empty(queue))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Arkanoid!");
+
+            // rysowanie platformy gracza
+            al_draw_bitmap(gracz, SCREEN_WIDTH/2, SCREEN_HEIGHT - 20, 0);
+
             al_flip_display();
 
             redraw = false;
@@ -48,6 +70,8 @@ int main()
     al_destroy_display(disp);
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
+
+    al_destroy_bitmap(gracz);
 
     return 0;
 }
