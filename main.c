c49342b9c6af162670e47af15c32ac49e2972d7b
p0js <ztraktor2@gmail.com>
2022-05-02 12:32:26 +0200

    Wstepna implementacja drzewa czworkowego i kolizji pilki z ceglami i platforma gracza

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 
 #define SZEROKOSC_EKRANU 800
 #define WYSOKOSC_EKRANU 600
-#define SZEROKOSC_CEGIELKI 80
-#define WYSOKOSC_CEGIELKI 50
-#define KOLUMNA_CEGIELEK 4
-#define WIERSZ_CEGIELEK 10
+#define SZEROKOSC_CEGIELKI 101
+#define WYSOKOSC_CEGIELKI 76
+#define KOLUMNA_CEGIELEK 2
+#define WIERSZ_CEGIELEK 4
 #define KEY_SEEN     1
 #define KEY_RELEASED 2
 
@@ -28,6 +28,8 @@ struct Gracz {
     int zycie;
     int szybkosc_gracza;
     int x_pozycja;
+    int y_pozycja;
+    int szerokosc_platformy;
 };
 
 struct Cegielki {
@@ -120,6 +122,157 @@ void pilka_kolizja_z_ramka(struct Pilka *pilka) {
     }
 }
 
+void pilka_kolizja_z_graczem(struct Pilka *pilka, struct Gracz gracz) {
+    bool pilka_w_szerokosci_platformy = pilka->x >= gracz.x_pozycja && pilka->x <= gracz.x_pozycja + gracz.szerokosc_platformy;
+    bool pilka_na_wysokosci_platformy = pilka->y + 9 >= WYSOKOSC_EKRANU - 20; // 9 = wysokosc pilki
+
+    if (pilka_w_szerokosci_platformy && pilka_na_wysokosci_platformy) {
+        if (pilka->ruch_dol) {
+            pilka->ruch_dol = false;
+        }
+        else {
+            pilka->ruch_dol = true;
+        }
+    }
+}
+
+// implementacja quadtree
+struct QuadTree {
+    int lewy_x, lewy_y;
+    int prawy_x, prawy_y;
+
+    struct Cegielki *cegielki;
+
+    struct QuadTree *pg; // prawy gorny itd
+    struct QuadTree *lg;
+    struct QuadTree *pd;
+    struct QuadTree *ld;
+};
+
+struct QuadTree* stworz_drzewo(int lewy_x, int lewy_y, int prawy_x, int prawy_y) {
+    struct QuadTree* drzewo = (struct QuadTree*)malloc(sizeof(struct QuadTree));
+
+    drzewo->lewy_x = lewy_x;
+    drzewo->lewy_y = lewy_y;
+    drzewo->prawy_x = prawy_x;
+    drzewo->prawy_y = prawy_y;
+
+    drzewo->cegielki = NULL;
+    drzewo->pg = NULL;
+    drzewo->lg = NULL;
+    drzewo->pd = NULL;
+    drzewo->ld = NULL;
+
+    return drzewo;
+}
+
+bool czy_jest_w_granicy_cegly(struct QuadTree *quadTree, struct Cegielki *cegielka) {
+    bool x_granica = cegielka->x_pozycja >= quadTree->lewy_x && cegielka->x_pozycja <= quadTree->prawy_x;
+    bool y_granica = cegielka->y_pozycja >= quadTree->lewy_y && cegielka->y_pozycja <= quadTree->prawy_y;
+
+    return x_granica && y_granica;
+}
+
+bool czy_jest_w_granicy_pilka(struct QuadTree *quadTree, struct Pilka *pilka) {
+    bool x_granica = pilka->x >= quadTree->lewy_x && pilka->x <= quadTree->prawy_x;
+    bool y_granica = pilka->y >= quadTree->lewy_y && pilka->y <= quadTree->prawy_y;
+
+    return x_granica && y_granica;
+}
+
+void dodaj_do_drzewa(struct QuadTree *quadTree, struct Cegielki *cegielka) {
+    if (cegielka == NULL) {
+        return;
+    }
+
+    if (!czy_jest_w_granicy_cegly(quadTree, cegielka)) {
+        return;
+    }
+
+    if (abs(quadTree->lewy_x - quadTree->prawy_x) <= SZEROKOSC_CEGIELKI && abs(quadTree->lewy_y - quadTree->prawy_y) <= WYSOKOSC_CEGIELKI) {
+        if (quadTree->cegielki == NULL) {
+            quadTree->cegielki = cegielka;
+            /*
+            printf("dodano cegle na %d\n", quadTree->lewy_x);
+            printf("dodano cegle na %d\n", quadTree->lewy_y);
+            printf("dodano cegle na %d\n", quadTree->prawy_x);
+            printf("dodano cegle na %d\n", quadTree->prawy_y);
+            printf("dodano cegle na %d\n", quadTree->cegielki->x_pozycja);
+            puts("----");
+            */
+        }
+        return;
+    }
+
+    if ((quadTree->lewy_x + quadTree->prawy_x)/2 >= cegielka->x_pozycja) {
+        if ((quadTree->lewy_y + quadTree->prawy_y)/2 >= cegielka->y_pozycja) {
+            if (quadTree->lg == NULL) {
+                quadTree->lg = stworz_drzewo(quadTree->lewy_x, quadTree->lewy_y, (quadTree->lewy_x + quadTree->prawy_x)/2, (quadTree->lewy_y + quadTree->prawy_y)/2);
+            }
+            dodaj_do_drzewa(quadTree->lg, cegielka);
+        }
+        else {
+            if (quadTree->ld == NULL) {
+                quadTree->ld = stworz_drzewo(quadTree->lewy_x, (quadTree->lewy_y + quadTree->prawy_y)/2, (quadTree->lewy_x + quadTree->prawy_x)/2, quadTree->prawy_y);
+            }
+            dodaj_do_drzewa(quadTree->ld, cegielka);
+        }
+    }
+    else {
+        if ((quadTree->lewy_y + quadTree->prawy_y)/2 >= cegielka->y_pozycja) {
+            if (quadTree->pg == NULL) {
+                quadTree->pg = stworz_drzewo((quadTree->lewy_x + quadTree->prawy_x)/2, quadTree->lewy_y, quadTree->prawy_x, (quadTree->lewy_y + quadTree->prawy_y)/2);
+            }
+            dodaj_do_drzewa(quadTree->pg, cegielka);
+        }
+        else {
+            if (quadTree->pd == NULL) {
+                quadTree->pd = stworz_drzewo((quadTree->lewy_x + quadTree->prawy_x)/2, (quadTree->lewy_y + quadTree->prawy_y)/2, quadTree->prawy_x, quadTree->prawy_y);
+            }
+            dodaj_do_drzewa(quadTree->pd, cegielka);
+        }
+    }
+}
+
+struct Cegielki* szukaj_w_drzewie(struct QuadTree *quadTree, struct Pilka *pilka) {
+    if (!czy_jest_w_granicy_pilka(quadTree, pilka)) {
+        return NULL;
+    }
+
+    if (quadTree->cegielki != NULL) {
+        return quadTree->cegielki;
+    }
+
+    if ((quadTree->lewy_x + quadTree->prawy_x)/2 >= pilka->x) {
+        if ((quadTree->lewy_y + quadTree->prawy_y)/2 >= pilka->y) {
+            if (quadTree->lg == NULL) {
+                return NULL;
+            }
+            return szukaj_w_drzewie(quadTree->lg, pilka);
+        }
+        else {
+            if (quadTree->ld == NULL) {
+                return NULL;
+            }
+            return szukaj_w_drzewie(quadTree->ld, pilka);
+        }
+    }
+    else {
+        if ((quadTree->lewy_y + quadTree->prawy_y)/2 >= pilka->y) {
+            if (quadTree->pg == NULL) {
+                return NULL;
+            }
+            return szukaj_w_drzewie(quadTree->pg, pilka);
+        }
+        else {
+            if (quadTree->pd == NULL) {
+                return NULL;
+            }
+            return szukaj_w_drzewie(quadTree->pd, pilka);
+        }
+    }
+}
+
 int main()
 {
     al_init();
@@ -148,10 +301,10 @@ int main()
     struct Ustawienia_gry ustawienia_gry = {1, false};
 
     // Ustawienia gracza
-    struct Gracz gracz = {3, 5, (int) SZEROKOSC_EKRANU/2};
+    struct Gracz gracz = {3, 5, (int) SZEROKOSC_EKRANU/2, WYSOKOSC_EKRANU - 20, 50};
 
     // Ustawienia pilki
-    struct Pilka pilka = {(int) SZEROKOSC_EKRANU/2, (int) WYSOKOSC_EKRANU/2, 3, false, true};
+    struct Pilka pilka = {200, 200, 3, false, true};
 
     // Ustawienia cegielek
     struct Cegielki *cegielki = (struct Cegielki *)malloc(WIERSZ_CEGIELEK * KOLUMNA_CEGIELEK * sizeof(struct Cegielki));
@@ -167,11 +320,48 @@ int main()
     {
         al_wait_for_event(queue, &event);
 
+        // drzewo quadtree
+        struct QuadTree* quadTree = stworz_drzewo(0, 0, SZEROKOSC_EKRANU, WYSOKOSC_EKRANU);
+
+        for(int i = 0; i < KOLUMNA_CEGIELEK; i++) {
+            for(int j = 0; j < WIERSZ_CEGIELEK; j++) {
+                dodaj_do_drzewa(quadTree, &cegielki[i*WIERSZ_CEGIELEK + j]);
+            }
+        }
+
+        // testowanie
+        for(int i = 0; i < KOLUMNA_CEGIELEK; i++) {
+            for(int j = 0; j < WIERSZ_CEGIELEK; j++) {
+                struct Pilka p_test = {cegielki[i*WIERSZ_CEGIELEK + j].x_pozycja + 100, cegielki[i*WIERSZ_CEGIELEK + j].y_pozycja + 37, 3, false, true};
+                if (szukaj_w_drzewie(quadTree, &p_test)) {
+                    //printf("%d\n", p_test.x);
+                }
+            }
+        }
+
+        struct Cegielki *trafiona_cegielka = szukaj_w_drzewie(quadTree, &pilka);
+        if (trafiona_cegielka != NULL) {
+            if (pilka.ruch_dol) {
+                pilka.ruch_dol = false;
+            }
+            else {
+                pilka.ruch_dol = true;
+            }
+            if (pilka.ruch_lewo) {
+                pilka.ruch_lewo = false;
+            }
+            else {
+                pilka.ruch_lewo = true;
+            }
+            printf("%d\n", trafiona_cegielka->x_pozycja);
+        }
+
         switch(event.type)
         {
             case ALLEGRO_EVENT_TIMER:
                 // pilka sterowanie
                 pilka_kolizja_z_ramka(&pilka);
+                pilka_kolizja_z_graczem(&pilka, gracz);
                 pilka.x = ruch_pilki_x(pilka);
                 pilka.y = ruch_pilki_y(pilka);
 
@@ -212,7 +402,7 @@ int main()
             al_draw_bitmap(grafiki.tlo, 0, 0, 0);
 
             // rysowanie platformy gracza
-            al_draw_bitmap(grafiki.platforma, gracz.x_pozycja, WYSOKOSC_EKRANU - 20, 0);
+            al_draw_bitmap(grafiki.platforma, gracz.x_pozycja, gracz.y_pozycja, 0);
 
             // rysowanie cegielek
             rysowanie_cegielek(cegielki, grafiki);
